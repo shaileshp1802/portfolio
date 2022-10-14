7b0021fe4140d004ca3cdff410eed3e75ecf401c
Shailesh Paliwal <shpaliwal2002@gmail.com>
2022-10-14 23:41:14 +0530

    Added custom cursor

--- index.js
+++ index.js
@@ -14,22 +14,53 @@ function opentab(tabname) {
 }
 
 
-var sidemenu = document.getElementById('sidemenu');
 
-var window_width = window.innerWidth;
+const scriptURL = "https://script.google.com/macros/s/AKfycbz_xBtXoZMXs4poKZjvddP5uCMtp-1Ez-XledvsRJFkOmwYX3HzpOzwsXlEYKTuKvLtEw/exec"
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
 
-console.log(window_width);
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = 'Message Sent <i class="fa-solid fa-check">'
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.log('Error!', error.message))
+})
 
-function openmenu() {
-    sidemenu.style.right = '0';
-    sidemenu.style.display = 'flex';
-    sidemenu.style.flexDirection = 'column';
-}
+const toTop = document.querySelector('.to-top');
 
-function closemenu() {
-    sidemenu.style.right = '-300px';
-    // sidemenu.style.display = 'none';
-}
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 100){
+        toTop.classList.add('active');
+    }
+    else{
+        toTop.classList.remove('active');
+    }
+})
+
+
+
+// var sidemenu = document.getElementById('sidemenu');
+
+// var window_width = window.innerWidth;
+
+// console.log(window_width);
+
+// function openmenu() {
+//     sidemenu.style.right = '0';
+//     sidemenu.style.display = 'flex';
+//     sidemenu.style.flexDirection = 'column';
+// }
+
+// function closemenu() {
+//     sidemenu.style.right = '-300px';
+//     // sidemenu.style.display = 'none';
+// }
 
 
 // const cursor = document.querySelector(".cursor");
