981843303eacf5da5fe02b26717bd3f264e886d1
Shailesh Paliwal <shpaliwal2002@gmail.com>
2023-03-02 15:01:50 +0530

    Preloader added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
 
+
+
+
+
+
 function opentab(tabname) {
     for (tablink of tablinks) {
         tablink.classList.remove("active-link")
